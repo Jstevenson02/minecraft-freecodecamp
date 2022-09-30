0fc0dad0034d6f3b70f799292f9ebcb044c4919e
Jacob <jacobryanstevenson@gmail.com>
2022-09-30 14:05:00 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { Physics } from '@react-three/cannon';
+import { Sky } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 
 function App() {
   return (
     <>
-      <div>Outside Canvas</div>
-      <Canvas></Canvas>
+      <Canvas>
+        <Sky sunPosition={[100, 100, 20]} />
+        <ambientLight intensity={0.5} />
+        <Physics>
+
+        </Physics>
+      </Canvas>
     </>
   );
 }
